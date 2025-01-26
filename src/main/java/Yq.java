19a1b3818d2528b17d52f8a3880e7acc457eddbe
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-01-26 19:28:00 +0800

    Make chatbot Yq echo the command that the user inputs
    The chatbot Yq will echo whatever command or line that the user inputs
    within a 'while' loop until the user inputs "bye" then the chatbot Yq
    will stop running.
    
    Add processForThreeSeconds() method
    
    This acts as a processing time for chatbot Yq whenever it receives a
    command from the user's input.

--- src/main/java/Yq.java
+++ src/main/java/Yq.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Yq {
     public static void main(String[] args) {
         String logo = " __   __    _________\n"
@@ -10,7 +13,32 @@ public class Yq {
         System.out.println();
         System.out.println("Hello! I'm Yq.");
         System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String line;
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            line = userInput.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println("    Ok. Processing...");
+                // Process for 3 seconds after receiving command from user.
+                processForThreeSeconds();
+
+                System.out.println("    " + line);
+                System.out.println("    Is there anything else I can do for you?");
+                System.out.println();
+            }
+        }
+        System.out.println("    Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Imitates the processing time of 3 seconds by sleeping for 3 seconds.
+     */
+    public static void processForThreeSeconds() {
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException exception) {}
     }
 }
