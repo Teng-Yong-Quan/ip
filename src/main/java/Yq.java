58eeb4dc717435f624cb857bb3b797656dfcce51
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-01-26 18:55:46 +0800

    Rename Duke.java to Yq.java
    Rename chatbot Duke to Yq
    
    Create statements that allow chatbot Yq to greet and exits

---
+++ src/main/java/Yq.java
@@ -0,0 +1,16 @@
+public class Yq {
+    public static void main(String[] args) {
+        String logo = " __   __    _________\n"
+                +     "|  | |  |  |   ___   |\n"
+                +     " \\     /   |  |  _|  |\n"
+                +     "  |   |    |  |_|    |\n"
+                +     "  |   |    |______   \\\n"
+                +     "  |___|           \\___\\\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println();
+        System.out.println("Hello! I'm Yq.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
