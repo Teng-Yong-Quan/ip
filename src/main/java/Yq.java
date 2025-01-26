0538a4fa6ca50403d8b0f1ae65102169ee4742a9
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-01-26 21:19:00 +0800

    Add the ability to store and display text
    The chatbot Yq is able to store the text entered by the user in a list
    and display the contents in the list as requested.

--- src/main/java/Yq.java
+++ src/main/java/Yq.java
@@ -15,20 +15,50 @@ public class Yq {
         System.out.println("What can I do for you?");
 
         String line;
+        // Create an empty main list.
+        String[] list = new String[0];
         Scanner userInput = new Scanner(System.in);
         while (true) {
             line = userInput.nextLine();
             if (line.equalsIgnoreCase("bye")) {
                 break;
+            } else if (line.equalsIgnoreCase("list")) {
+                System.out.println("    Ok. Processing...");
+                // Process for 3 seconds after receiving command from user.
+                processForThreeSeconds();
+                System.out.println();
+
+                if (list.length == 0) {
+                    System.out.println("    The list is empty. There is nothing to show.");
+                } else {
+                    for (int i = 0; i < list.length; i++) {
+                        if (list[i] != null) {
+                            System.out.println("    " + Integer.toString(i + 1) + ": " + list[i]);
+                        }
+                    }
+                }
             } else {
                 System.out.println("    Ok. Processing...");
                 // Process for 3 seconds after receiving command from user.
                 processForThreeSeconds();
-
-                System.out.println("    " + line);
-                System.out.println("    Is there anything else I can do for you?");
                 System.out.println();
+
+                // Create a new list that is longer than the main list by 1.
+                String[] newList = new String[list.length + 1];
+
+                // Transfer all items in the main list into the new list.
+                System.arraycopy(list, 0, newList, 0, list.length);
+
+                // Add latest item into the new list.
+                newList[newList.length - 1] = line;
+
+                // Set new list as the main list.
+                list = newList;
+                System.out.println("    added: " + line);
             }
+            System.out.println();
+            System.out.println("    Is there anything else I can do for you?");
+            System.out.println();
         }
         System.out.println("    Bye. Hope to see you again soon!");
     }
