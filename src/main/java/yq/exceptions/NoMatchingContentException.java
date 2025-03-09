542f2b28befa202daa3c67d1128861dbc9693e2b
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-09 18:06:27 +0800

    Edit and shorten the error message for NoMatchingContentException

--- src/main/java/yq/exceptions/NoMatchingContentException.java
+++ src/main/java/yq/exceptions/NoMatchingContentException.java
@@ -11,9 +11,8 @@ public class NoMatchingContentException extends YqException {
     public String getMessage() {
         return """
                     Unable to find tasks with '%s' in the task descriptions.
-                    If the tasks with '%s' are actually present in the list, please key in only the
-                    relevant words that you are searching for with the correct spelling in uppercase or lowercase when
-                    necessary as it is case-sensitive.
-                """.formatted(commandInput, commandInput);
+                    If it is due to incorrect spelling, please key in only the relevant words that
+                    you are searching for with the correct spelling (case-insensitive).
+                """.formatted(commandInput);
     }
 }
